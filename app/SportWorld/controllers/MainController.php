6fb3351fb2ca1a46c6d122cb5b1b639ac7b62694
Luis <luis.amengual@sitrack.com>
2014-03-07 17:15:18 -0300

    Commit inicial (estructura)

---
+++ app/SportWorld/controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SportWorld\controllers;
+
+use NeoPHP\web\WebController;
+
+class MainController extends WebController 
+{
+    public function indexAction ()
+    {
+        echo "TODO";
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once ("bootstrap.php");
+$app = new SportWorld\WebApplication();
+$app->start();
+
+?>
